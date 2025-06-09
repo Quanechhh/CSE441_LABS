54d7d24fcfbdd5b5d2ce1d72143aba388cd903b3
Quanechhh <quan16143@gmail.com>
2025-06-09 17:21:25 +0700

    Lab_11

---
+++ Lab_11_project2/app/src/main/java/com/example/lab_11_project2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.lab_11_project2;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            Intent intent = getIntent();
+            Uri data = intent.getData();
+            try {
+                WebView webView = findViewById(R.id.webview1);
+                webView.loadUrl(data.toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return insets;
+        });
+    }
+}
